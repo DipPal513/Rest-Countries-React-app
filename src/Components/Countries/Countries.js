b2f4ea24fccb7083c0c0973cd7f95f16d1d81b72
DipPal513 <dip.pal.513@gmail.com>
2021-09-20 11:31:11 +0600

    first comit

---
+++ src/Components/Countries/Countries.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import function Countries() {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch("https://restcountries.eu/rest/v2/all")
+            .then((res) => res.json())
+            .then((data) => setCountries(data));
+    }, []);
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>countries available: {countries.length}</h1>
+            {countries.map((country) => {
+                return <Country name={country.name} region={country.region} />;
+            })}
+        </div>
+    );
+};
+
+
+  
+export default Countries;
---
+++ src/Components/Countries/Country.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Country = () => {
+  return (
+    <div>
+      <h5>region : {props.region}</h5>
+      <h3>name: {props.name}</h3>
+    </div>
+  );
+};
